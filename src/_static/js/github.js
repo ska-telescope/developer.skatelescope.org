284e8dc73b55c949d53b88a8b15bdb1e92d59490
Marco Bartolini <m.bartolini@skatelescope.org>
2018-10-08 12:23:15 +0100

    refined github scraping

--- src/_static/js/github.js
+++ src/_static/js/github.js
@@ -1,12 +1,26 @@
 jQuery(function(){
-    var list = $("#list-of-projects > ul");
-    console.log(list);
     var github_v3_endpoint = "https://api.github.com/orgs/ska-telescope/repos";
-    $.getJSON(github_v3_endpoint, function(data){
-        $.each(data, function(key, val){
-           console.log(val);
-           item = "<li>" + val["name"] + "</li>";
-           $(item).appendTo(list);
-        }); //end each
-    }); //end getJSON
+    var list = $("#list-of-projects > ul");
+    if( list.length ){
+        list.empty();
+        $.getJSON(github_v3_endpoint, function(data){
+            $.each(data, function(key, val){
+               //console.log(val);
+               if(val["description"])
+                   description = val["description"];
+               else
+                   description = "";
+               item = "<li><a href=\"" +
+                      val["html_url"] +
+                      "\">" +
+                      val["name"] +
+                      "</a> " +
+                      description +
+                      "</li>";
+               $(item).appendTo(list);
+            }); //end each
+        }); //end getJSON
+    }else{ //if list not found
+        //console.log("list not found")
+    }
 });
--- src/projects/list.rst
+++ src/projects/list.rst
@@ -1,3 +1,7 @@
+# this title is converted into a DOM id and used
+# for populating this page using Github APIs,
+# Do not edit it
+
 List of projects
 ----------------
 
