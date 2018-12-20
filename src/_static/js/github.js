669488fe82ebe8a9a6769d9becceace3750d82c4
Matteo04052017 <matteodicarlo@gmail.com>
2018-12-20 10:18:17 +0100

    dynamic ci dashboard

--- src/_static/js/github.js
+++ src/_static/js/github.js
@@ -23,4 +23,12 @@ jQuery(function(){
     }else{ //if list not found
         //console.log("list not found")
     }
+	
+	var ci_dashboard = "https://ska-telescope.gitlab.io/ska_ci_dashboard/";
+    var dashboard_table = $("#ci-dashboard > table");
+    if( dashboard_table.length ){
+        $.get(ci_dashboard, function(data){
+			dashboard_table.html( data);
+        });
+    }
 });
