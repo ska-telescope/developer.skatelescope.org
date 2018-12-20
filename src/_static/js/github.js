32d9088426782ed898317447a2fe2928b73eeec8
Matteo04052017 <matteodicarlo@gmail.com>
2018-12-20 10:25:42 +0100

    add only the table

--- src/_static/js/github.js
+++ src/_static/js/github.js
@@ -28,7 +28,7 @@ jQuery(function(){
     var dashboard_table = $("#ci-dashboard > table");
     if( dashboard_table.length ){
         $.get(ci_dashboard, function(data){
-			dashboard_table.html( data);
+			dashboard_table.html($(data).find("#dataTable"));
         });
     }
 });
