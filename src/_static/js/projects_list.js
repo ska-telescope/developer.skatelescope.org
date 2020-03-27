4442626eb1be98fa96634b0262e72e18e1af512e
Matteo Canzari <matteo.canzari@gmail.com>
2020-03-27 17:32:28 +0100

    Update projects_list.js
    modified projects list

--- src/_static/js/projects_list.js
+++ src/_static/js/projects_list.js
@@ -19,6 +19,12 @@
                   docs_name = path.replace(/\./g,"");
                   if(name == "developer.skatelescope.org")
                       docs_url = "https://developer.skatelescope.org";
+                  else if(name == "web-maxiv-tangogql")
+                      docs_url = "https://web-maxiv-tangogql.readthedocs.io";
+                  else if(name == "webjive")
+                      docs_url = "https://webjive.readthedocs.io";
+                  else if(name == "dashboard-repo")
+                      docs_url = "https://webjive-dashboards.readthedocs.io";
                    else
                        docs_url = "https://developer.skatelescope.org/projects/" + docs_name;
                   item ="<tr>" +
