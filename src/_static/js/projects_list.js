640b86917cf0bfa77a8083985b32c30fe9e7e46e
dalmiromaia <dmaia@fc.up.pt>
2020-05-04 14:45:32 +0100

    adding templates and sdi projects to projects_list.js

--- src/_static/js/projects_list.js
+++ src/_static/js/projects_list.js
@@ -1,11 +1,21 @@
    jQuery(function(){
+       var ska = "https://gitlab.com/api/v4/groups/3180705/projects?per_page=100&order_by=name&sort=asc&simple=true";
+       var templ = "https://gitlab.com/api/v4/groups/5901724/projects?per_page=100&order_by=name&sort=asc&simple=true";
+       var sdi = "https://gitlab.com/api/v4/groups/7480052/projects?per_page=100&order_by=name&sort=asc&simple=true";
        var gitlab_v4_endpoint = "https://gitlab.com/api/v4/groups/3180705/projects?per_page=100&order_by=name&sort=asc&simple=true";
        var list = $("#list-of-projects tbody");
        var i = 1;
        //console.log(list);
+       //
        if( list.length ){
            list.empty();
-           $.getJSON(gitlab_v4_endpoint, function(data){
+           // this nested approach works fine and it is easy to understand but if there are more urls involved 
+           // then it should be changed to use $.when 
+           //
+           $.getJSON(sdi, function(data1){
+            $.getJSON(templ, function(data2){
+             $.getJSON(ska, function(data3){
+               data = data1.concat(data2,data3);
                $.each(data, function(key, val){
 //                  console.log(val);
                   i = i+1;
@@ -41,6 +51,8 @@
                       $(item).appendTo(list);
                }); //end each
               console.log(i + " projects");
+           }); //end getJSON
+           }); //end getJSON
            }); //end getJSON
        }else{ //if list not found
            console.log("list not found")
