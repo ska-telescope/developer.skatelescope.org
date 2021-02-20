8c9416bae3caf6f6087b39a2d7324e5c965c7be5
dalmiromaia <dmaia@fc.up.pt>
2021-02-20 17:24:53 +0000

    SP-1466 Override css defaults for table

--- src/_static/js/topics_list.js
+++ src/_static/js/topics_list.js
@@ -1,4 +1,9 @@
    jQuery(function(){
+   // table header defaults for the theme
+   var tbstart = "<table class=\"docutils align-default\"  style=\"width: 100%;\"><colgroup>" +
+                 "<col style=\"width: 20%;\" /><col style=\"width: 80%;\" /></colgroup><thead>" + 
+                 "<tr class=\"row-odd\"><th class=\"head\"  style=\"width: 20%;\"><p>Documentation</p></th>" +
+                 "<th class=\"head\"  style=\"width: 80%;\"><p>Gitlab repository</p></th></tr></thead><tbody>" ;
        var checklist = $("#software-modules-list p:eq(0)").text();
        var checklist = checklist.replace(/\s/g,''); // remove white space
        var checklist = checklist.split(':'); // split Topics from the tags
@@ -8,7 +13,7 @@
        var pg2 = "https://gitlab.com/api/v4/groups/3180705/projects?per_page=100&simple=true&archived=false&include_subgroups=true&all_available=true&page=2";
        var pg3 = "https://gitlab.com/api/v4/groups/3180705/projects?per_page=100&simple=true&archived=false&include_subgroups=true&all_available=true&page=3";
        var readthedocs_prepend="ska-telescope-"; // all the readthedocs projects start with ska-telescope- next to their name
-       var list = $("#software-modules-list tbody");
+       var list = $("#software-modules-list table");
        if( list.length && topics === "Topics" && checklist.length){
            list.empty();
            // concatenate all pages
@@ -39,10 +44,11 @@
                   item ="<tr>" +
                        "<td><a href=\"" + docs_url + "/en/latest/?badge=latest\" >" +
                            "<img src=\"https://readthedocs.org/projects/" + readthedocs_prepend + 
-                         docs_name + "/badge/?version=latest\" alt=\"Documentation Status\" style=\"width:50%;\" /> " +  "</a></td>" +
+                         docs_name + "/badge/?version=latest\" alt=\"Documentation Status\" style=\"width:100%;\" /> " +  "</a></td>" +
                         "<td><a alt=\"repo url on gitlab\" href=\"" + gitlab_url + "\">" + name + "</a><br>" + description +
                        "</td>" +
                         "</tr>";
+                  item = tbstart + item + "</tbody></table></div>";        
                        $(item).appendTo(list);
                    } //end if for overlist
                }); //end each
